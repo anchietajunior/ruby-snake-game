b8087cf9fe92eaeb0b080c24744e76dace009d2c
Anchieta <santosjr87@gmail.com>
2020-08-03 13:39:09 -0300

    Add movement to snake removing the last piece each frame and adding a new one to the head

--- snake.rb
+++ snake.rb
@@ -1,6 +1,7 @@
 require 'ruby2d'
 
 set background: 'navy'
+set fps_cap: 1
 
 # width = 640 / 20 = 32
 # height = 480 / 20 = 24
@@ -10,16 +11,43 @@ GRID_SIZE = 20
 class Snake
   def initialize
     @positions = [[2, 0], [2, 1], [2, 2], [2, 3]]
+    @direction = 'down'
   end
 
   def draw
     @positions.each do |position|
-      Square.new(x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE, size: GRID_SIZE - 1, color: 'white')
+      Square.new(
+        x: position[0] * GRID_SIZE,
+        y: position[1] * GRID_SIZE, 
+        size: GRID_SIZE - 1, 
+        color: 'white'
+      )
     end
   end
+
+  def move
+    @positions.shift
+    case @direction
+    when 'down'
+      @positions.push([head[0], head[1] + 1])
+    end
+  end
+
+  private
+
+  def head
+    @positions.last
+  end
+
 end
 
 snake = Snake.new
-snake.draw
+
+update do
+  clear
+
+  snake.move
+  snake.draw
+end
 
 show
